555fa96039539345af088a908f2227e63bb1359d
Johnmark-Enriquez <john.e@appyugo.com>
2021-05-01 06:10:56 +0800

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Vote;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -9,16 +11,20 @@ class DashboardController extends Controller
     public function index()
     {
 
-        $results = DB::table('votes')
-        ->select('candidate_id',
-                'candidates.id',
-                'candidates.firstname',
-                'candidates.lastname',
-                DB::raw('count(votes.id) as votes_count, candidate_id'))
-        ->join('candidates','candidates.id','=','candidate_id')
+        // $results = DB::table('votes')
+        // ->select('candidate_id',
+        //         // 'candidates.id',
+        //         // 'candidates.firstname',
+        //         // 'candidates.lastname',
+        //         DB::raw('count(votes.id) as votes_count'))
+        // // ->join('candidates','candidates.id','=','candidate_id')
+        // ->groupBy('candidate_id')
+        // ->get();
+        $results = Vote::select("candidate_id", DB::raw("count(votes.id) as votes_count"))
         ->groupBy('candidate_id')
+        ->with('candidate')
         ->get();
-        // dd($results);
+       
         // $results = DB::table('votes')->count();
         // dd($results);
         // dd($results);
