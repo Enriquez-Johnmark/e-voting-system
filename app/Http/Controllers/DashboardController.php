719e1c86c2f014558d44057590af88114830def0
Johnmark-Enriquez <john.e@appyugo.com>
2021-05-01 06:00:06 +0800

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,15 +10,19 @@ class DashboardController extends Controller
     {
 
         $results = DB::table('votes')
-        ->select('votes.candidate_id',
+        ->select('candidate_id',
                 'candidates.id',
                 'candidates.firstname',
                 'candidates.lastname',
-                DB::raw('count(votes.id) as votes_count'))
-        ->join('candidates','candidates.id','=','votes.candidate_id')
+                DB::raw('count(votes.id) as votes_count, candidate_id'))
+        ->join('candidates','candidates.id','=','candidate_id')
         ->groupBy('candidate_id')
         ->get();
         // dd($results);
+        // $results = DB::table('votes')->count();
+        // dd($results);
+        // dd($results);
         return view('admin.dashboard' ,compact('results'));
+
     }
 }
